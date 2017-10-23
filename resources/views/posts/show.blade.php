c194801ad11d7256398124483e12043d66046df3
d3z41k <d3z41k@gmail.com>
2017-10-23 15:17:41 +0200

    16 Add Comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,24 +10,45 @@
 
         <hr>
 
-        <ul class="list-group">
-
         <div class="comments">
-            @foreach($post->comments as $comment)
+            <ul class="list-group">
 
-                <li class="list-group-item">
-                    <strong>
-                        {{ $comment->created_at->diffForHumans() }}: &nbsp;
-                    </strong>
+                    @foreach($post->comments as $comment)
 
+                        <li class="list-group-item">
+                            <strong>
+                                {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                            </strong>
 
-                    {{ $comment->body }}
-                </li>
+                            {{ $comment->body }}
+                        </li>
 
-            @endforeach
+                    @endforeach
+            </ul>
         </div>
 
-        </ul>
+        <hr>
+
+        <div class="card">
+            <div class="card-block">
+
+                <form method="POST" action="/posts/{{ $post->id }}/comments">
+
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your comment here..." class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add comment</button>
+                    </div>
+                </form>
+
+                @include('layouts.errors')
+
+            </div>
+        </div>
 
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,7 @@ Route::get('/tasks/{task}', 'TaskController@show');
 
 
 Route::get('/', 'PostsController@index');
-
 Route::get('/posts/create', 'PostsController@create');
-
-Route::post('/posts', 'PostsController@store');
-
 Route::get('/posts/{post}', 'PostsController@show');
+Route::post('/posts', 'PostsController@store');
+Route::post('/posts/{post}/comments', 'CommentsController@store');
