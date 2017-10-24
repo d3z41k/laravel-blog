a5e1f589b7e7d318252189708d2996f6baa93cbd
d3z41k <d3z41k@gmail.com>
2017-10-24 13:41:29 +0200

    21 View Composers

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -24,15 +24,8 @@ class PostsController extends Controller
         } else {
             $posts = Post::latest()->get();
         }
-
-
-        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at) desc')
-            ->get()
-            ->toArray();
         
-        return view('posts.index', compact(['posts', 'archives']));
+        return view('posts.index', compact('posts'));
     }
 
     public function create()
--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -24,7 +24,7 @@ class RegistrationController extends Controller
         $user = User::create([
             'name' => request('name'),
             'email' => request('email'),
-            'password'=> bcrypt(request('email'))
+            'password'=> bcrypt(request('password'))
         ]);
 
         auth()->login($user);
